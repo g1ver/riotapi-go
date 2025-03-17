adfc1237d42a639c9ebf3da115b47ffb9b298866
giver <g1ver@tuta.io>
2025-03-17 12:24:21 -0700

    implement lolstatus

--- client/lolstatus.go
+++ client/lolstatus.go
@@ -1,3 +1,19 @@
 package client
 
-// lolstatus.go is a generated Go file from the directory lolstatus.go
+import (
+	"fmt"
+
+	"github.com/g1ver/riotapi-go/models"
+)
+
+// Get League of Legends status for the given platform.
+func (c *Client) GetLeagueStatus() (*models.PlatformData, error) {
+	url := fmt.Sprintf("%s/lol/status/v4/platform-data", c.baseUrl)
+
+	var pd models.PlatformData
+	if err := c.Get(url, &pd); err != nil {
+		return nil, fmt.Errorf("failed to get league status: %w", err)
+	}
+
+	return &pd, nil
+}
--- models/lolstatus.go
+++ models/lolstatus.go
@@ -1,3 +1,36 @@
 package models
 
-// lolstatus.go is a generated Go file from the directory lolstatus.go
+type Content struct {
+	Locale  string `json:"locale"`
+	Content string `json:"content"`
+}
+
+type Update struct {
+	ID               int       `json:"id"`
+	Author           string    `json:"author"`
+	Publish          bool      `json:"publish"`
+	PublishLocations []string  `json:"publish_locations"`
+	Translations     []Content `json:"translations"`
+	CreatedAt        string    `json:"created_at"`
+	UpdatedAt        string    `json:"updated_at"`
+}
+
+type Status struct {
+	ID                int       `json:"id"`
+	MaintenanceStatus string    `json:"maintenance_status"`
+	IncidentSeverity  string    `json:"incident_severity"`
+	Titles            []Content `json:"titles"`
+	Updates           []Update  `json:"updates"`
+	CreatedAt         string    `json:"created_at"`
+	ArchiveAt         string    `json:"archive_at"`
+	UpdatedAt         string    `json:"updated_at"`
+	Platforms         []string  `json:"platforms"`
+}
+
+type PlatformData struct {
+	ID           string   `json:"id"`
+	Name         string   `json:"name"`
+	Locales      []string `json:"locales"`
+	Maintenances []Status `json:"maintenances"`
+	Incidents    []Status `json:"incidents"`
+}
