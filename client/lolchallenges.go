ce48846ec845ec520c926667b47719f39e59e68e
giver <g1ver@tuta.io>
2025-03-17 12:10:13 -0700

    dont need lol-challenges for now

--- client/lolchallenges.go
+++
@@ -1,3 +0,0 @@
-package client
-
-// lolchallenges.go is a generated Go file from the directory lolchallenges.go
--- models/lolchallenges.go
+++
@@ -1,3 +0,0 @@
-package models
-
-// lolchallenges.go is a generated Go file from the directory lolchallenges.go
