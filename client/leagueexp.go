9fb088448b641bcb080316d5c0c3bed4e120f4a1
giver <g1ver@tuta.io>
2025-03-17 10:23:29 -0700

    implement league-exp routes

--- client/leagueexp.go
+++ client/leagueexp.go
@@ -1,3 +1,21 @@
 package client
 
-// leagueexp.go is a generated Go file from the directory leagueexp.go
+import (
+	"fmt"
+
+	"github.com/g1ver/riotapi-go/models"
+)
+
+// Get all the league entries
+// Technically, these are all unique players and Riot returns a Set
+// I'm just going to use a slice for now
+func (c *Client) GetLeagueEntries(queue string, tier string, division string, page int) ([]models.LeagueEntry, error) {
+	url := fmt.Sprintf("%s/lol/league-exp/v4/entries/%s/%s/%s?page=%d", c.baseUrl, queue, tier, division, page)
+
+	var les []models.LeagueEntry
+	if err := c.Get(url, &les); err != nil {
+		return nil, fmt.Errorf("failed to get league entries: %w", err)
+	}
+
+	return les, nil
+}
--- models/leagueexp.go
+++ models/leagueexp.go
@@ -1,3 +1,25 @@
 package models
 
-// leagueexp.go is a generated Go file from the directory leagueexp.go
+type LeagueEntry struct {
+	LeagueID     string     `json:"leagueId"`
+	SummonerID   string     `json:"summonerId"`
+	PUUID        string     `json:"puuid"`
+	QueueType    string     `json:"queueType"`
+	Tier         string     `json:"tier"`
+	Rank         string     `json:"rank"`
+	LeaguePoints int        `json:"leaguePoints"`
+	Wins         int        `json:"wins"`
+	Losses       int        `json:"losses"`
+	HotStreak    bool       `json:"hotStreak"`
+	Veteran      bool       `json:"veteran"`
+	FreshBlood   bool       `json:"freshBlood"`
+	Inactive     bool       `json:"inactive"`
+	MiniSeries   MiniSeries `json:"miniSeries"`
+}
+
+type MiniSeries struct {
+	Losses   int    `json:"losses"`
+	Progress string `json:"progress"`
+	Target   int    `json:"target"`
+	Wins     int    `json:"wins"`
+}
