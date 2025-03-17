5ab3f67af86b9e865434cd5f4b0a0baa1224534f
giver <g1ver@tuta.io>
2025-03-17 15:06:36 -0700

    finish spectator endpoint

--- client/spectator.go
+++ client/spectator.go
@@ -1,3 +1,31 @@
 package client
 
-// spectator.go is a generated Go file from the directory spectator.go
+import (
+	"fmt"
+
+	"github.com/g1ver/riotapi-go/models"
+)
+
+// Get current game information for the given puuid.
+func (c *Client) GetActiveGameInfoByPUUID(puuid string) (*models.CurrentGameInfo, error) {
+	u := fmt.Sprintf("%s/lol/spectator/v5/active-games/by-summoner/%s", c.baseUrl, puuid)
+
+	var gameInfo models.CurrentGameInfo
+	if err := c.Get(u, &gameInfo); err != nil {
+		return nil, err
+	}
+
+	return &gameInfo, nil
+}
+
+// Get a list of featured games.
+func (c *Client) GetFeaturedGames() (*models.FeaturedGames, error) {
+	u := fmt.Sprintf("%s/lol/spectator/v5/featured-games", c.baseUrl)
+
+	var featuredGames models.FeaturedGames
+	if err := c.Get(u, &featuredGames); err != nil {
+		return nil, err
+	}
+
+	return &featuredGames, nil
+}
--- models/spectator.go
+++ models/spectator.go
@@ -1,3 +1,78 @@
 package models
 
-// spectator.go is a generated Go file from the directory spectator.go
+type CurrentGameInfo struct {
+	GameID            int64                    `json:"gameId"`
+	GameType          string                   `json:"gameType"`
+	GameStartTime     int64                    `json:"gameStartTime"`
+	MapID             int64                    `json:"mapId"`
+	GameLength        int64                    `json:"gameLength"`
+	PlatformID        string                   `json:"platformId"`
+	GameMode          string                   `json:"gameMode"`
+	BannedChampions   []BannedChampion         `json:"bannedChampions"`
+	GameQueueConfigID int64                    `json:"gameQueueConfigId"`
+	Observers         Observer                 `json:"observers"`
+	Participants      []CurrentGameParticipant `json:"participants"`
+}
+
+type BannedChampion struct {
+	PickTurn   int   `json:"pickTurn"`
+	ChampionID int64 `json:"championId"`
+	TeamID     int64 `json:"teamId"`
+}
+
+type Observer struct {
+	EncryptionKey string `json:"encryptionKey"`
+}
+
+type CurrentGameParticipant struct {
+	ChampionID               int64                     `json:"championId"`
+	Perks                    SpectatorPerks            `json:"perks"`
+	ProfileIconID            int64                     `json:"profileIconId"`
+	Bot                      bool                      `json:"bot"`
+	TeamID                   int64                     `json:"teamId"`
+	SummonerID               string                    `json:"summonerId"`
+	PUUID                    string                    `json:"puuid"`
+	Spell1ID                 int64                     `json:"spell1Id"`
+	Spell2ID                 int64                     `json:"spell2Id"`
+	GameCustomizationObjects []GameCustomizationObject `json:"gameCustomizationObjects"`
+}
+
+type SpectatorPerks struct {
+	PerkIDs      []int64 `json:"perkIds"`
+	PerkStyle    int64   `json:"perkStyle"`
+	PerkSubStyle int64   `json:"perkSubStyle"`
+}
+
+type GameCustomizationObject struct {
+	Category string `json:"category"`
+	Content  string `json:"content"`
+}
+
+type FeaturedGames struct {
+	GameList              []FeaturedGameInfo `json:"gameList"`
+	ClientRefreshInterval int64              `json:"clientRefreshInterval"`
+}
+
+type FeaturedGameInfo struct {
+	GameMode          string                 `json:"gameMode"`
+	GameLength        int64                  `json:"gameLength"`
+	MapID             int64                  `json:"mapId"`
+	GameType          string                 `json:"gameType"`
+	BannedChampions   []BannedChampion       `json:"bannedChampions"`
+	GameID            int64                  `json:"gameId"`
+	Observers         Observer               `json:"observers"`
+	GameQueueConfigID int64                  `json:"gameQueueConfigId"`
+	Participants      []SpectatorParticipant `json:"participants"`
+	PlatformID        string                 `json:"platformId"`
+}
+
+type SpectatorParticipant struct {
+	Bot           bool   `json:"bot"`
+	Spell2ID      int64  `json:"spell2Id"`
+	ProfileIconID int64  `json:"profileIconId"`
+	SummonerID    string `json:"summonerId"`
+	PUUID         string `json:"puuid"`
+	ChampionID    int64  `json:"championId"`
+	TeamID        int64  `json:"teamId"`
+	Spell1ID      int64  `json:"spell1Id"`
+}
