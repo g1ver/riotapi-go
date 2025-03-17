6c7e031eb85d207e4b86f00161554904b695ebfc
giver <g1ver@tuta.io>
2025-03-17 15:33:49 -0700

    finish summoner endpoint

--- client/summoner.go
+++ client/summoner.go
@@ -1,3 +1,43 @@
 package client
 
-// summoner.go is a generated Go file from the directory summoner.go
+import (
+	"fmt"
+
+	"github.com/g1ver/riotapi-go/models"
+)
+
+// Get a summoner by account id.
+func (c *Client) GetSummonerByAccountID(accountId string) (*models.Summoner, error) {
+	url := fmt.Sprintf("%s/lol/summoner/v4/summoners/by-account/%s", c.baseUrl, accountId)
+
+	var summoner models.Summoner
+	if err := c.Get(url, &summoner); err != nil {
+		return nil, err
+	}
+
+	return &summoner, nil
+}
+
+// Get a summoner by PUUID.
+func (c *Client) GetSummonerByPUUID(puuid string) (*models.Summoner, error) {
+	url := fmt.Sprintf("%s/lol/summoner/v4/summoners/by-puuid/%s", c.baseUrl, puuid)
+
+	var summoner models.Summoner
+	if err := c.Get(url, &summoner); err != nil {
+		return nil, err
+	}
+
+	return &summoner, nil
+}
+
+// Get a summoner by summoner ID.
+func (c *Client) GetSummonerBySummonerID(summonerId string) (*models.Summoner, error) {
+	url := fmt.Sprintf("%s/lol/summoner/v4/summoners/%s", c.baseUrl, summonerId)
+
+	var summoner models.Summoner
+	if err := c.Get(url, &summoner); err != nil {
+		return nil, err
+	}
+
+	return &summoner, nil
+}
--- models/summoner.go
+++ models/summoner.go
@@ -1,3 +1,10 @@
 package models
 
-// summoner.go is a generated Go file from the directory summoner.go
+type Summoner struct {
+	AccountID     string `json:"accountId"`
+	ProfileIconID int    `json:"profileIconId"`
+	RevisionDate  int64  `json:"revisionDate"`
+	ID            string `json:"id"`
+	PUUID         string `json:"puuid"`
+	SummonerLevel int64  `json:"summonerLevel"`
+}
