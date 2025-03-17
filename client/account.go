f00b79a78184fa3f6294be066d3698fb57f6dac2
giver <g1ver@tuta.io>
2025-03-17 15:31:14 -0700

    remove unused param

--- client/account.go
+++ client/account.go
@@ -19,7 +19,7 @@ func (c *Client) GetAccountByPUUID(puuid string) (*models.Account, error) {
 }
 
 // Get account by riot id
-func (c *Client) GetAccountByRiotID(gameName string, tagLine string, region string) (*models.Account, error) {
+func (c *Client) GetAccountByRiotID(gameName string, tagLine string) (*models.Account, error) {
 	url := fmt.Sprintf("%s/riot/account/v1/accounts/by-riot-id/%s/%s", c.routingServerUrl, gameName, tagLine)
 
 	var account models.Account
