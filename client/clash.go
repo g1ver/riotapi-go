07e23a909c8a76f64fe5f4d46e24532c5fe698d6
giver <g1ver@tuta.io>
2025-03-17 09:08:51 -0700

    implement clash routes

--- client/clash.go
+++ client/clash.go
@@ -1,3 +1,67 @@
 package client
 
-// clash.go is a generated Go file from the directory clash.go
+import (
+	"fmt"
+
+	"github.com/g1ver/riotapi-go/models"
+)
+
+// Get players by puuid.
+func (c *Client) GetClashPlayersByPUUID(puuid string) ([]models.Player, error) {
+	url := fmt.Sprintf("%s/lol/clash/v1/players/by-puuid/%s", c.baseUrl, puuid)
+
+	var ps []models.Player
+	if err := c.Get(url, ps); err != nil {
+		return nil, fmt.Errorf("failed to get player by puuid: %w", err)
+	}
+
+	return ps, nil
+}
+
+// Get team by ID.
+func (c *Client) GetClashTeamByID(teamId string) (*models.Team, error) {
+	url := fmt.Sprintf("%s/lol/clash/v1/teams/%s", c.baseUrl, teamId)
+
+	var t models.Team
+	if err := c.Get(url, &t); err != nil {
+		return nil, fmt.Errorf("failed to get team by id: %w", err)
+	}
+
+	return &t, nil
+}
+
+// Get all active or upcoming tournaments
+func (c *Client) GetClashTournaments() ([]models.Tournament, error) {
+	url := fmt.Sprintf("%s/lol/clash/v1/tournaments", c.baseUrl)
+
+	var ts []models.Tournament
+	if err := c.Get(url, ts); err != nil {
+		return nil, fmt.Errorf("failed to get team by id: %w", err)
+	}
+
+	return ts, nil
+}
+
+// Get tournament by team ID
+func (c *Client) GetClashTournamentByTeamID(teamId string) (*models.Tournament, error) {
+	url := fmt.Sprintf("%s/lol/clash/v1/tournaments/by-team/%s", c.baseUrl, teamId)
+
+	var t models.Tournament
+	if err := c.Get(url, t); err != nil {
+		return nil, fmt.Errorf("failed to get team by id: %w", err)
+	}
+
+	return &t, nil
+}
+
+// Get tournament by ID
+func (c *Client) GetClashTournamentByID(tournamentId string) (*models.Tournament, error) {
+	url := fmt.Sprintf("%s/lol/clash/v1/tournaments/%s", c.baseUrl, tournamentId)
+
+	var t models.Tournament
+	if err := c.Get(url, t); err != nil {
+		return nil, fmt.Errorf("failed to get team by id: %w", err)
+	}
+
+	return &t, nil
+}
--- models/clash.go
+++ models/clash.go
@@ -1,3 +1,35 @@
 package models
 
-// clash.go is a generated Go file from the directory clash.go
+type Player struct {
+	SummonerId string `json:"summonerId"`
+	PUUID      string `json:"puuid"`
+	TeamId     string `json:"teamId"`
+	Position   string `json:"position"`
+	Role       string `json:"role"`
+}
+
+type Team struct {
+	ID           string   `json:"id"`
+	TournamentID int      `json:"tournamentId"`
+	Name         string   `json:"name"`
+	IconID       int      `json:"iconId"`
+	Tier         int      `json:"tier"`
+	Captain      string   `json:"captain"`
+	Abbreviation string   `json:"abbreviation"`
+	Players      []Player `json:"players"`
+}
+
+type Tournament struct {
+	ID               int               `json:"id"`
+	ThemeID          int               `json:"themeId"`
+	NameKey          string            `json:"nameKey"`
+	NameKeySecondary string            `json:"nameKeySecondary"`
+	Schedule         []TournamentPhase `json:"schedule"`
+}
+
+type TournamentPhase struct {
+	ID               int64 `json:"id"`
+	RegistrationTime int64 `json:"registrationTime"`
+	StartTime        int64 `json:"startTime"`
+	Cancelled        bool  `json:"cancelled"`
+}
