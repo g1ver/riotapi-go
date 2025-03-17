08743783e6cb1c82b2a9f3afcae0d74e84844920
giver <g1ver@tuta.io>
2025-03-17 12:05:35 -0700

    clarify endpoint

--- client/leagueexp.go
+++ client/leagueexp.go
@@ -9,7 +9,7 @@ import (
 // Get all the league entries
 // Technically, these are all unique players and Riot returns a Set
 // I'm just going to use a slice for now
-func (c *Client) GetLeagueEntries(queue string, tier string, division string, page int) ([]models.LeagueEntry, error) {
+func (c *Client) GetAllLeagueExpEntries(queue string, tier string, division string, page int) ([]models.LeagueEntry, error) {
 	url := fmt.Sprintf("%s/lol/league-exp/v4/entries/%s/%s/%s?page=%d", c.baseUrl, queue, tier, division, page)
 
 	var les []models.LeagueEntry
